c4fd1bee22a9938a3380134fea4ff41de96e82cd
Sumanbitw <007sumanghosh@gmail.com>
2021-08-28 21:18:20 +0530

    added images to a folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Blog from "./components/blog/Blog";
+import Brand from "./components/brands/Brand";
+import Features from "./components/features/Features";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+import Task from "./components/task/Task";
+import Teams from "./components/teams/Teams";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Main />
+      <Brand />
+      <Task />
+      <Features />
+      <Teams/>
+      <Blog/>
+      <Footer/>
     </div>
   );
 }
