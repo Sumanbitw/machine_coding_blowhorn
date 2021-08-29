16bfc46b05b9b2585078fca735628b3729fc9647
Sumanbitw <007sumanghosh@gmail.com>
2021-08-29 17:46:34 +0530

    final fixes

--- src/components/brands/Brand.jsx
+++ src/components/brands/Brand.jsx
@@ -1,41 +1,61 @@
-import React from 'react'
-import disney from "../../assets/brands/disney.png"
-import google from "../../assets/brands/google.png"
-import hubspot from "../../assets/brands/hubspot.png"
-import slack from "../../assets/brands/slack.png"
-import youtube from "../../assets/brands/youtube.png"
-import shopify from "../../assets/brands/shopify.png"
+import React from "react";
+import disney from "../../assets/brands/disney.png";
+import google from "../../assets/brands/google.png";
+import hubspot from "../../assets/brands/hubspot.png";
+import slack from "../../assets/brands/slack.png";
+import youtube from "../../assets/brands/youtube.png";
+import shopify from "../../assets/brands/shopify.png";
 
 function Brand() {
   return (
-    <section class="bg-white pt-7 pb-14">
-      <div class="container px-8 mx-auto sm:px-12 lg:px-20">
-        <h1 class="text-lg font-bold tracking-wide text-center text-gray-800 uppercase mb-7">
+    <section className="bg-white pt-7 pb-14">
+      <div className="container px-8 mx-auto sm:px-12 lg:px-20">
+        <h1 className="text-lg font-bold tracking-wide text-center text-gray-800 uppercase mb-7">
           TRUSTED BY THE TOP-LEADING COMPANIES
         </h1>
-      <div class="flex grid items-center justify-center grid-cols-4 grid-cols-12 gap-y-8"> 
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={disney} alt="Disney Plus" class="block object-contain h-12"/>
+        <div className="flex grid items-center justify-center grid-cols-4 grid-cols-12 gap-y-8">
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img
+              src={disney}
+              alt="Disney Plus"
+              className="block object-contain h-12"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img
+              src={google}
+              alt="Google"
+              className="block object-contain h-20"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img
+              src={hubspot}
+              alt="Hubspot"
+              className="block object-contain h-20"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img
+              src={youtube}
+              alt="Youtube"
+              className="block object-contain h-20 lg:h-20"
+            />
+          </div>
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img src={slack} alt="Slack" className="block object-contain h-9" />
+          </div>
+          <div className="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
+            <img
+              src={shopify}
+              alt="Shopify"
+              className="block object-contain h-9"
+            />
+          </div>
         </div>
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={google} alt="Google" class="block object-contain h-20"/>
-        </div>
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={hubspot} alt="Hubspot" class="block object-contain h-20"/>
-        </div>
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={youtube} alt="Youtube" class="block object-contain h-20 lg:h-20"/>
-        </div>
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={slack} alt="Slack" class="block object-contain h-9"/>
-        </div>
-        <div class="flex items-center justify-center col-span-6 sm:col-span-4 md:col-span-3 xl:col-span-2">
-          <img src={shopify} alt="Shopify" class="block object-contain h-9"/>
-        </div>
-      </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Brand
+export default Brand;
--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 function Features() {
   return (
-    <section class="py-20 bg-white">
-      <div class="container max-w-6xl mx-auto">
-        <h2 class="text-4xl font-bold tracking-tight text-center">
+    <section className="py-20 bg-white">
+      <div className="container max-w-6xl mx-auto">
+        <h2 className="text-4xl font-bold tracking-tight text-center">
           Our Features
         </h2>
-        <p class="mt-2 text-lg text-center text-gray-600">
+        <p className="mt-2 text-lg text-center text-gray-600">
           Check out our list of awesome features below.
         </p>
 
-        <div class="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+        <div className="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -31,17 +31,19 @@ function Features() {
                 <path d="M4.5 17l-1.5 5l3 -1.5l3 1.5l-1.5 -5"></path>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Certifications</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">
+              Certifications
+            </h4>
+            <p className="text-base text-center text-gray-500">
               Each of our plan will provide you and your team with
               certifications.
             </p>
           </div>
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -55,16 +57,16 @@ function Features() {
                 <path d="M12 8h0l4.524 -3.77a0.9 .9 0 0 1 1.476 .692v12.156a0.9 .9 0 0 1 -1.476 .692l-4.524 -3.77h-8a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h8"></path>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Notifications</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">Notifications</h4>
+            <p className="text-base text-center text-gray-500">
               Send out notifications to all your customers to keep them engaged.
             </p>
           </div>
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -80,16 +82,16 @@ function Features() {
                 <line x1="16" y1="5.25" x2="8" y2="9.75"></line>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Bundles</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">Bundles</h4>
+            <p className="text-base text-center text-gray-500">
               High-quality bundles of awesome tools to help you out.
             </p>
           </div>
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -103,17 +105,19 @@ function Features() {
                 <rect x="3" y="4" width="18" height="16" rx="2"></rect>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Developer tools</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">
+              Developer tools
+            </h4>
+            <p className="text-base text-center text-gray-500">
               Developer tools to help grow your application and keep it
               up-to-date.
             </p>
           </div>
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -128,17 +132,19 @@ function Features() {
                 <path d="M7 5h1v-2h8v2h1a3 3 0 0 1 3 3v9a3 3 0 0 1 -3 3v1h-10v-1a3 3 0 0 1 -3 -3v-9a3 3 0 0 1 3 -3"></path>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Building Blocks</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">
+              Building Blocks
+            </h4>
+            <p className="text-base text-center text-gray-500">
               The right kind of building blocks to take your company to the next
               level.
             </p>
           </div>
-          <div class="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
-            <div class="p-3 text-white bg-blue-500 rounded-full">
+          <div className="relative flex flex-col items-center justify-between col-span-4 px-8 py-12 space-y-4 overflow-hidden bg-gray-100 sm:rounded-xl">
+            <div className="p-3 text-white bg-blue-500 rounded-full">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="w-8 h-8 "
+                className="w-8 h-8 "
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
@@ -153,8 +159,8 @@ function Features() {
                 <path d="M5 5h14a2 2 0 0 1 2 2v3a2 2 0 0 0 0 4v3a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-3a2 2 0 0 0 0 -4v-3a2 2 0 0 1 2 -2"></path>
               </svg>
             </div>
-            <h4 class="text-xl font-medium text-gray-700">Coupons</h4>
-            <p class="text-base text-center text-gray-500">
+            <h4 className="text-xl font-medium text-gray-700">Coupons</h4>
+            <p className="text-base text-center text-gray-500">
               Coupons system to provide special offers and discounts for your
               app.
             </p>
